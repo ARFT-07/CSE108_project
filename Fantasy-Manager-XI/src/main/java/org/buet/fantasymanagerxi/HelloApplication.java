10d0200b655a760c7782cbfd1051c092c5fd4209
ARFT_07 <adiyasir0@gmail.com>
2026-02-11 22:28:10 +0600

    icon

--- Fantasy-Manager-XI/src/main/java/org/buet/fantasymanagerxi/HelloApplication.java
+++ Fantasy-Manager-XI/src/main/java/org/buet/fantasymanagerxi/HelloApplication.java
@@ -3,17 +3,34 @@ package org.buet.fantasymanagerxi;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+
+
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Fantasy Manager XI");
         stage.setScene(scene);
+
+        Image icon =
+                   new Image(HelloApplication.class.getResourceAsStream("img.png"));
+        stage.getIcons().add(icon);
+
         stage.show();
     }
 }
