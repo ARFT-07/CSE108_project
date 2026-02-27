2258310f23b9b0c8ad6774a6249c8602a1c0dd02
z-zarif <rahatzarif0443@gmail.com>
2026-02-27 07:35:24 +0600

    done with the skeleton of homepage, need to update the documentation and add css files

--- Fantasy-Manager-XI/src/main/java/org/buet/fantasymanagerxi/HomeController.java
+++ Fantasy-Manager-XI/src/main/java/org/buet/fantasymanagerxi/HomeController.java
@@ -2,22 +2,45 @@ package org.buet.fantasymanagerxi;
 
 //package com.fantasymanagerxi.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.beans.property.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import org.buet.fantasymanagerxi.model.*;
 import org.buet.fantasymanagerxi.service.DataService;
+
+import java.io.File;
+import java.net.URL;
+
 public class HomeController {
 
-    @FXML private ListView<Team> teamListView;
-    @FXML private TableView<LeagueTableEntry> leagueTableView;
+    @FXML
+    private ListView<Team> teamListView;
+    @FXML
+    private TableView<LeagueTableEntry> leagueTableView;
 
-    @FXML private TableColumn<LeagueTableEntry, Integer> positionCol;
-    @FXML private TableColumn<LeagueTableEntry, String> teamCol;
-    @FXML private TableColumn<LeagueTableEntry, Integer> pointsCol;
-    @FXML private TableColumn<LeagueTableEntry, Integer> gdCol;
+    @FXML
+    private TableColumn<LeagueTableEntry, Integer> positionCol;
+    @FXML
+    private TableColumn<LeagueTableEntry, String> teamCol;
+    @FXML
+    private TableColumn<LeagueTableEntry, Integer> pointsCol;
+    @FXML
+    private TableColumn<LeagueTableEntry, Integer> gdCol;
 
-    @FXML private Label highlightLabel;
+    @FXML
+    private Label highlightLabel;
+    @FXML
+    private StackPane videoPane;
+    @FXML
+    private MediaView mediaView;
+    private MediaPlayer mediaPlayer;
+
 
     private DataService dataService = new DataService();
 
@@ -26,7 +49,36 @@ public class HomeController {
 
         setupTeamList();
         setupLeagueTable();
-        setupHighlights();
+        //setupHighlights();
+        setupMediaPlayer();
+
+    }
+
+    private void setupMediaPlayer() {
+        URL mediaUrl = getClass().getResource("/org/buet/fantasymanagerxi/data/fcb.mp4");
+        // String path = new File("src/main/resources/org/buet/fantasymanagerxi/data/fcb.mp4").toURI().toString();
+        if (mediaUrl == null) {
+            System.out.println("File not found");
+            return;
+        }
+
+        Media media = new Media(mediaUrl.toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setAutoPlay(false);
+//        mediaView.fitWidthProperty().bind(videoPane.widthProperty());
+//        mediaView.fitHeightProperty().bind(videoPane.heightProperty());
+//        mediaView.setPreserveRatio(true);
+    }
+
+    @FXML
+    private void handlePlay() {
+        mediaPlayer.play();
+    }
+
+    @FXML
+    private void handlePause() {
+        mediaPlayer.pause();
     }
 
     private void setupTeamList() {
@@ -59,10 +111,9 @@ public class HomeController {
         leagueTableView.setItems(dataService.getLeagueTable());
     }
 
-    private void setupHighlights() {
-
-        highlightLabel.setText(
-                "Matchday 1: Arsenal 3 - 1 Chelsea | City 2 - 0 Liverpool"
-        );
+    @FXML
+    private void movetoplayerDb(ActionEvent actionEvent) {
+        return;
     }
+
 }
