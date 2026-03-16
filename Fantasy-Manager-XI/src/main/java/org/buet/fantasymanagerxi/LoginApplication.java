c5833bacad66560f67b744dcc3f72af31ac0267c
z-zarif <rahatzarif0443@gmail.com>
2026-03-16 15:24:49 +0600

    updated player db

--- Fantasy-Manager-XI/src/main/java/org/buet/fantasymanagerxi/LoginApplication.java
+++ Fantasy-Manager-XI/src/main/java/org/buet/fantasymanagerxi/LoginApplication.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class LoginApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login_SignupApplication.class.getResource("/org/buet/fantasymanagerxi/fxml/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Login_SignupApplication.class.getResource("/org/buet/fantasymanagerxi/fxml/player-db.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         scene.getStylesheets().add(getClass().getResource("/org/buet/fantasymanagerxi/css/login_style.css").toExternalForm());
         Image img = (Image) new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/buet/fantasymanagerxi/images/logo.png")));
